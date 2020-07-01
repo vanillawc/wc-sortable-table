b2bc5f31d7a293cea6e27891e6e088486a6dafea
Evan <evanplaice@gmail.com>
2020-06-30 20:47:09 -0600

    Update Build Workflow
    - remove rollup-plugin-terser
    - delete rollup configs
    - upgrade esmtk
    - update scripts
    - update .npmignore
    - update build

--- .config/main.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'src/wc-sortable-table.js',
-  output: {
-    file: 'index.js',
-    format: 'esm'
-  }
-};
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-/* eslint-disable no-new-func */
-/**
- * Interpolate a tagged template literal from the inputs
- *
- * @param {*} template the template literal string
- * @param {*} [tags] the tagged values in the template
- * @returns the template output with the tagged literals applied
- */
-function interpolate (template, tags = {}) {
+// node_modules/@vanillaes/interpolate/index.js
+function interpolate(template, tags = {}) {
   const keys = Object.keys(tags);
   const values = Object.values(tags);
   try {
@@ -15,50 +8,47 @@ function interpolate (template, tags = {}) {
     throw new TemplateException(template, tags, e);
   }
 }
-
-/**
- * @private
- */
 class TemplateException extends Error {
-  constructor (template, tags, message) {
+  constructor(template, tags, message) {
     super();
-    this.name = 'TemplateError';
-    let msg = '\n------------------\n';
+    this.name = "TemplateError";
+    let msg = "\n------------------\n";
     msg += `Template: \`${template}\``;
-    msg += '\n------------------\n';
+    msg += "\n------------------\n";
     msg += `Tags: ${JSON.stringify(tags, null, 2)}`;
-    msg += '\n------------------\n';
+    msg += "\n------------------\n";
     msg += message;
     this.message = msg;
   }
 }
 
-/* eslint no-undef: 0 */
-
+// src/wc-sortable-table.js
 class WCSortableTable extends HTMLElement {
-  static get observedAttributes () {
-    return ['src']
+  static get observedAttributes() {
+    return ["src"];
   }
-
-  attributeChangedCallback (name, oldValue, newValue) {
-    if (!this.__initialized) { return }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (!this.__initialized) {
+      return;
+    }
     if (oldValue !== newValue) {
       this[name] = newValue;
     }
   }
-
-  get src () { return this.getAttribute('src') }
-  set src (value) {
-    this.setAttribute('src', value);
+  get src() {
+    return this.getAttribute("src");
+  }
+  set src(value) {
+    this.setAttribute("src", value);
     this.setSrc(value);
   }
-
-  get value () { return this.__data }
-  set value (value) {
+  get value() {
+    return this.__data;
+  }
+  set value(value) {
     this.setValue(value);
   }
-
-  constructor () {
+  constructor() {
     super();
     this.__initialized = false;
     this.__theme = null;
@@ -68,81 +58,70 @@ class WCSortableTable extends HTMLElement {
     this.__headers = null;
     this.__selected = null;
   }
-
-  async connectedCallback () {
-    this.__table = document.createElement('table');
-
-    if (this.hasAttribute('theme')) {
-      this.__theme = document.createElement('template');
+  async connectedCallback() {
+    this.__table = document.createElement("table");
+    if (this.hasAttribute("theme")) {
+      this.__theme = document.createElement("template");
       this.__theme.innerHTML = await this.getTheme();
-      this.attachShadow({ mode: 'open' });
+      this.attachShadow({mode: "open"});
       this.shadowRoot.appendChild(this.__theme.content.cloneNode(true));
       this.shadowRoot.appendChild(this.__table);
     } else {
       this.appendChild(this.__table);
     }
-
-    if (this.hasAttribute('src')) {
+    if (this.hasAttribute("src")) {
       this.setSrc();
     }
-
     this.__initialized = true;
   }
-
-  async getTheme () {
-    const path = this.getAttribute('theme');
+  async getTheme() {
+    const path = this.getAttribute("theme");
     const contents = await this.fetchTheme(path);
-    return interpolate(contents)
+    return interpolate(contents);
   }
-
-  async fetchTheme (src) {
+  async fetchTheme(src) {
     const response = await fetch(src);
-    if (response.status !== 200) throw Error(`ERR ${response.status}: ${response.statusText}`)
-    return response.text()
+    if (response.status !== 200)
+      throw Error(`ERR ${response.status}: ${response.statusText}`);
+    return response.text();
   }
-
-  async setSrc () {
-    if (this.hasAttribute('src')) {
+  async setSrc() {
+    if (this.hasAttribute("src")) {
       this.__data = await this.fetchSrc(this.src);
       this.render();
     }
   }
-
-  async fetchSrc (src) {
+  async fetchSrc(src) {
     const response = await fetch(src);
-    if (response.status !== 200) throw Error(`ERR ${response.status}: ${response.statusText}`)
-    return response.json()
+    if (response.status !== 200)
+      throw Error(`ERR ${response.status}: ${response.statusText}`);
+    return response.json();
   }
-
-  setValue (value) {
+  setValue(value) {
     this.__data = value;
     this.render();
   }
-
-  sortData (data) {
-    if (this.__column === null) { return data }
+  sortData(data) {
+    if (this.__column === null) {
+      return data;
+    }
     const col = this.__column;
     const dir = this.__direction;
-    const comp = dir
-      ? (a, b) => a < b
-      : (a, b) => a > b;
+    const comp = dir ? (a, b) => a < b : (a, b) => a > b;
     return data.sort((a, b) => {
-      return comp(a[col], b[col]) ? -1 : 1
-    })
+      return comp(a[col], b[col]) ? -1 : 1;
+    });
   }
-
-  headerClicked (e) {
+  headerClicked(e) {
     this.__selected = e.target.cellIndex;
     const column = e.target.cellIndex;
-    this.__direction = (column !== this.__column)
-      ? true : !this.__direction;
+    this.__direction = column !== this.__column ? true : !this.__direction;
     this.__column = column;
     this.render();
   }
-
-  styleHeaders () {
-    const dir = this.__direction ? 'asc' : 'desc';
-    const notDir = this.__direction ? 'asc' : 'desc';
+  styleHeaders() {
+    const dir = this.__direction ? "asc" : "desc";
+    const notDir = this.__direction ? "asc" : "desc";
     for (const th of this.__headers.children) {
       if (th.cellIndex === this.__selected) {
         if (th.classList.contains(notDir)) {
@@ -160,41 +139,33 @@ class WCSortableTable extends HTMLElement {
       }
     }
   }
-
-  render () {
+  render() {
     let data = [...this.__data];
-    const table = document.createElement('table');
-    this.__headers = document.createElement('tr');
-    this.__headers.addEventListener('click', e => this.headerClicked(e));
-
-    // build the headers row
+    const table = document.createElement("table");
+    this.__headers = document.createElement("tr");
+    this.__headers.addEventListener("click", (e) => this.headerClicked(e));
     const headers = data.shift();
-    headers.forEach(header => {
-      const th = document.createElement('th');
+    headers.forEach((header) => {
+      const th = document.createElement("th");
       th.innerText = header;
       this.__headers.appendChild(th);
     });
     this.styleHeaders();
-    const thead = document.createElement('thead');
+    const thead = document.createElement("thead");
     thead.appendChild(this.__headers);
     table.appendChild(thead);
-
-    // sort the values
     data = this.sortData(data);
-
-    // build the data rows
-    const tbody = document.createElement('tbody');
-    data.forEach(row => {
-      const tr = document.createElement('tr');
-      row.forEach(cell => {
-        const td = document.createElement('td');
+    const tbody = document.createElement("tbody");
+    data.forEach((row) => {
+      const tr = document.createElement("tr");
+      row.forEach((cell) => {
+        const td = document.createElement("td");
         td.innerText = cell;
         tr.appendChild(td);
       });
       tbody.appendChild(tr);
     });
     table.appendChild(tbody);
-
     if (this.__theme) {
       this.shadowRoot.removeChild(this.__table);
       this.__table = table;
@@ -206,7 +177,7 @@ class WCSortableTable extends HTMLElement {
     }
   }
 }
-
-customElements.define('wc-sortable-table', WCSortableTable);
-
-export { WCSortableTable };
+customElements.define("wc-sortable-table", WCSortableTable);
+export {
+  WCSortableTable
+};
